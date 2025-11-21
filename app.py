8528e019cd40661a7fbf6cdc4f5103cfb7d0004a
conconq <github.h3pee@avantamail.com>
2025-11-21 12:55:34 -0500

    Update app.py
    Added 60 second game timer and countdown

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-# TypeQuest - Sprint 2 start
+import time
 
 print("Welcome to TypeQuest!")
 print("A privacy-first offline typing game.\n")
 
 input("Press ENTER to begin...")
+
+print("You will have 60 seconds to type words.\n")
+
+start_time = time.time()
+time_limit = 60
+
+while time.time() - start_time < time_limit:
+    time_left = int(time_limit - (time.time() - start_time))
+    print(f"Time left: {time_left} seconds", end="\r")
+
+print("\nTime is up!")
